890922710f366fdc8a661de5903b924df5d6692d
Aistis <62752446+Aistis225@users.noreply.github.com>
2020-03-27 18:10:48 +0200

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,17 @@
+/*
+function addContent(){
+  alert('Nice');
+  console.log("Clicked");
+  document.getElementById("add-button").removeEventListener("click", addContent);
+}
+document.getElementById("add-button").addEventListener("click", addContent);
+*/
+
+/*
+document.getElementById("add-button").onclick = function () {
+    var div = document.createElement('div');
+       div.className = 'panel-content-page';
+
+       document.getElementsByTagName('body')[0].appendChild(div);
+};
+*/
